3022b0b0c320692c06a4ee94c369d57d8321c6b6
SawSandy <sawsandi.sgg@gmail.com>
2022-06-06 12:23:48 +0630

    ForLoopStatement

---
+++ src/controlflow/ForLoopStatement.java
@@ -0,0 +1,18 @@
+package controlflow;
+
+public class ForLoopStatement {
+    public static void main(String[] args) {
+        //for(3statement){}
+        //1=> initialize statement;
+        //2=> condition statement;
+        //3=> count++ --;
+
+        for(int i=0; i<10; i++){
+            System.out.print(i+ "\t");
+
+        }
+
+
+    }
+
+}
